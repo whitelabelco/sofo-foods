73f5b2c898fa7ff8ee2eb22c16a822cac937a48e
CiaraMaria <mariaverse@protonmail.com>
2024-09-12 11:50:28 -0600

    test input

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+"use client";
+
+import Input from "./components/Input";
+
 export default function Home() {
-  return <div className="text-3xl">Sofo Foods</div>;
+  return <div className="flex justify-center items-center py-20">
+    <Input variant="emailList" placeholder="Enter your email" onChange={(e) => console.log(e.target.value)} value="" />
+  </div>;
 }
