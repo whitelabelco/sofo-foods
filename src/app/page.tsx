dd4eef54d2745d2bdc41107720af873e08ff027e
CiaraMaria <mariaverse@protonmail.com>
2024-09-12 11:16:43 -0600

    test link

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,7 @@
+import Link from "./components/Link";
+
 export default function Home() {
-  return <div className="text-3xl">Sofo Foods</div>;
+  return <div className="flex items-center justify-center">
+    <Link variant="sidebar">Link</Link>
+  </div>;
 }
