edbc854e8a0744811738ea8aa137c4c11130f71c
CiaraMaria <mariaverse@protonmail.com>
2024-09-12 17:17:27 -0600

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+"use client";
+
+import FooterLink from "./components/FooterLink";
+
 export default function Home() {
-  return <div className="text-3xl">Sofo Foods</div>;
+  return <div className="flex justify-center items-center py-20 bg-black">
+    <FooterLink href="/" onClick={() => console.log("clicked")} arrow>Footer</FooterLink>
+  </div>;
 }
