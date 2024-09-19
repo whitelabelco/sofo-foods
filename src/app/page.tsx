2685888b906d3d58787d374e4da0a7e5fed66c03
CiaraMaria <mariaverse@protonmail.com>
2024-09-18 20:40:13 -0600

    test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,8 @@
 
-import ContactCallout from "./components/ContactCallout";
+import Button from "./components/Button";
 
 export default function Home() {
   return <div className="flex justify-center items-center py-20">
-    <ContactCallout />
+    <Button variant="blue" size="base">become a customer</Button>
   </div>;
 }
