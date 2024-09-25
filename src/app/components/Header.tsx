272a83f8bcc76d32a66da24a2976822f1c2c1168
CiaraMaria <mariaverse@protonmail.com>
2024-09-25 08:01:17 -0600

    refactor re-used styles

--- src/app/components/Header.tsx
+++ src/app/components/Header.tsx
@@ -1,6 +1,8 @@
 "use client";
 
 import React, { useState } from 'react';
+import { useRouter } from 'next/navigation'
+import Image from 'next/image';
 
 import MenuSvg from '/public/icons/menu.svg';
 import CloseSvg from '/public/icons/x.svg';
@@ -8,18 +10,31 @@ import Link from './Link';
 import Button from './Button';
 
 const Header = () => {
+    const router = useRouter()
     const [isOpen, setIsOpen] = useState(false);
+
+    const navLinks = [
+        <Link key={0} href="/about">About Us</Link>,
+        <Link key={1} href="/brands">Brands</Link>,
+        <Link key={2} href="/products">Products</Link>,
+        <Link key={3} href="/careers">Careers</Link>,
+        <Link key={4} href="/order">Order Now</Link>
+    ]
+
+    const headerLogo = {
+        mobile: <a href="/"><Image src="/images/logo-horizontal.svg" alt="logo" width={288} height={33} className="mb-2 w-48"/></a>,
+        desktop: <a href="/"><Image src="/images/logo-horizontal.svg" alt="logo" width={288} height={48} className="my-4 w-72"/></a>
+    }
 
   return (
     <>
-        {/* sm */}
-        <div className="md:hidden flex flex-col items-center bg-white border-b border-darkest/10 drop-shadow-sm gap-4">
-        <div className="h-2 lg:h-8 bg-red flex-none order-0 self-stretch"></div>
-            <nav className="flex flex-row w-full justify-between items-center px-2">
+        <div className="xl:hidden flex flex-col items-center bg-white border-b border-darkest/10 drop-shadow-sm gap-4">
+        <div className="xl:hidden h-2 bg-red flex-none order-0 self-stretch"></div>
+
+            {/* sm */}
+            <nav className="md:hidden flex flex-row w-full justify-between items-center px-2">
                 <div>
-                    <a href="/">
-                        <img src="/images/logo-horizontal.svg" alt="logo" className="mb-2 w-48"/>
-                    </a>
+                    {headerLogo.mobile}
                 </div>
                 <div className="space-y-2" onClick={() => setIsOpen((prev) => !prev)}>
                     <MenuSvg />
@@ -29,89 +44,53 @@ const Header = () => {
                     <CloseSvg />
                 </div>
                     <ul className="flex flex-col items-center justify-between min-h-[400px] py-12">
-                        <li>
-                            <Link href="/about">About Us</Link>
-                        </li>
-                        <li>
-                            <Link href="/brands">Brands</Link>
-                        </li>
-                        <li>
-                            <Link href="/products">Products</Link>
-                        </li>
-                        <li>
-                            <Link href="/careers">Careers</Link>
-                        </li>
-                        <li>
-                            <Link href="/order">Order Now</Link>
-                        </li>
+                        {navLinks.map((link, index) => (
+                            <li key={index}>{link}</li>
+                        ))}
                     </ul>
                 </div>
             </nav>
-        </div>
 
-        {/* md */}
-        <div className="hidden lg:hidden md:flex flex-col items-center bg-white border-b border-darkest/10 drop-shadow-sm gap-4">
-        <div className="h-2 lg:h-8 bg-red flex-none order-0 self-stretch"></div>
-            <nav className="flex flex-col w-full justify-between items-center">
+            {/* md/lg */}
+            <nav className="hidden xl:hidden md:flex flex-col w-full justify-between items-center">
                 <div className="flex justify-center w-full border-b border-darkest/10">
-                    <a href="/">
-                        <img src="/images/logo-horizontal.svg" alt="logo" className="my-4 w-72"/>
-                    </a>
+                    {headerLogo.desktop}
                 </div>
                     <ul className="flex flex-row items-center justify-between min-w-full px-12 py-6">
-                    <li>
-                            <Link href="/about">About Us</Link>
-                        </li>
+                        {navLinks.map((link, index) => (
+                            <li key={index}>{link}</li>
+                        ))}
                         <li>
-                            <Link href="/brands">Brands</Link>
-                        </li>
-                        <li>
-                            <Link href="/products">Products</Link>
-                        </li>
-                        <li>
-                            <Link href="/careers">Careers</Link>
-                        </li>
-                        <li>
-                            <Link href="/order">Order Now</Link>
-                        </li>
-                        <li>
-                            <Button variant="blue" size="sm">Get in touch</Button>
+                            <Button onClick={() => router.push('/contact')} variant="blue" size="sm">Get in touch</Button>
                         </li>
                     </ul>
             </nav>
         </div>
 
-        {/* lg */}
-        <div className="hidden lg:flex flex-col items-center bg-white border-b border-darkest/10 drop-shadow-sm">
-        <div className="h-2 lg:h-8 bg-red flex flex-row self-stretch items-center justify-end pr-14">
+        {/* xl */}
+        <div className="hidden xl:flex flex-col items-center bg-white border-b border-darkest/10 drop-shadow-sm">
+        <div className="h-8 bg-red flex flex-row self-stretch items-center justify-end pr-14">
             <Link href="/" className="text-white underline text-xs">Customer Login</Link>
         </div>
             <nav className="flex flex-row w-full justify-between items-center">
-                    <ul className="flex flex-row items-center justify-between w-1/3 px-12 py-6">
-                        <li>
-                            <Link href="/about">About Us</Link>
-                        </li>
-                        <li>
-                            <Link href="/brands">Brands</Link>
-                        </li>
-                        <li>
-                            <Link href="/products">Products</Link>
-                        </li>
+                    <ul className="flex flex-row items-center justify-between w-1/3 px-6 py-6">
+                        {navLinks.map((link, index) => (
+                            index <= 2 && (
+                                <li key={index}>{link}</li>
+                            )
+                        ))}
                     </ul>
                     <div>
-                        <a href="/">
-                            <img src="/images/logo-horizontal.svg" alt="logo" className="my-4 w-72"/>
-                        </a>
+                        {headerLogo.desktop}
                     </div>
-                    <ul className="flex flex-row items-center justify-between w-1/3 px-12 py-6">
+                    <ul className="flex flex-row items-center justify-between w-1/3 px-4 py-6">
+                        {navLinks.map((link, index) => (
+                            index >= 3 && (
+                                <li key={index}>{link}</li>
+                            )
+                        ))}
                         <li>
-                            <Link href="/careers">Careers</Link>
-                        </li>
-                        <li>
-                            <Link href="/order">Order Now</Link>
-                        </li>
-                        <li>
-                            <Button variant="blue" size="base">Become a customer</Button>
+                            <Button onClick={() => router.push('/contact')} variant="blue" size="base">Become a customer</Button>
                         </li>
                     </ul>
             </nav>
