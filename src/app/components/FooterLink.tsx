b7c8b86da07703fc2001f721df7db1a145bcdb2c
CiaraMaria <mariaverse@protonmail.com>
2024-09-26 07:03:37 -0600

    remove onClick prop from footer links

--- src/app/components/FooterLink.tsx
+++ src/app/components/FooterLink.tsx
@@ -7,13 +7,12 @@ interface FooterLinkProps {
   children: React.ReactNode;
   href?: string;
   arrow?: boolean;
-  onClick: () => void;
 }
 
-const FooterLink: React.FC<FooterLinkProps> = ({ children, href = "/", arrow, onClick }) => {
+const FooterLink: React.FC<FooterLinkProps> = ({ children, href = "/", arrow }) => {
 
   return (
-        <NextLink href={href} onClick={onClick} className="group flex items-center justify-center gap-1 whitespace-nowrap font-normal text-lg text-white/70 hover:text-white hover:ease-out hover:duration-150">
+        <NextLink href={href} className="group flex items-center justify-center gap-1 whitespace-nowrap font-normal text-lg text-white/70 hover:text-white hover:ease-out hover:duration-150">
             {children}
             {arrow && 
                 <ArrowSvg className="stroke-white/70 group-hover:stroke-white" />
