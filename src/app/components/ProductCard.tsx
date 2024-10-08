78df467f11216017decc7786f7d2ead20c926dd2
CiaraMaria <mariaverse@protonmail.com>
2024-10-08 16:46:28 -0600

    change padding to account for animation

--- src/app/components/ProductCard.tsx
+++ src/app/components/ProductCard.tsx
@@ -13,7 +13,7 @@ const ProductCard: React.FC<ProductCardProps> = ({ name }) => {
     <div className={`bg-product-placeholder bg-cover w-[180px] h-[148px] md:w-[450px] md:h-[300px] flex flex-col relative gap-6 rounded-lg md:rounded-2xl`}>
         <div className="bg-darkest bg-opacity-0 p-4 w-full h-full hover:bg-opacity-50 transition-all duration-200 rounded-lg md:rounded-2xl cursor-pointer">
             <div className="absolute bottom-0 left-0">
-                    <Button onClick={() => console.log("clicked")} variant="product" className="!px-2">{name}</Button>
+                    <Button onClick={() => console.log("clicked")} variant="product" className="!px-4">{name}</Button>
             </div>
         </div>
     </div>
