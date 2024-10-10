a046dd2d809b62ff9b207e3116a589958d9b248f
CiaraMaria <mariaverse@protonmail.com>
2024-10-09 20:18:07 -0600

    Update tailwind.config.ts

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -35,6 +35,8 @@ const config: Config = {
         'product-placeholder': "url('/images/product-placeholder.jpeg')",
         'products-callout': "linear-gradient(rgba(0, 0, 0, 0) 90%, rgba(255, 255, 255, 255)), url('/images/products-callout.jpg')",
         'careers': "linear-gradient(rgba(0, 0, 0, 0) 30%, rgba(25, 25, 23, 1)",
+        'brands-hero': "url('/images/brands-hero.webp')",
+        'products-hero': "url('/images/products-hero.webp')",
       }
     },
     borderWidth: {
