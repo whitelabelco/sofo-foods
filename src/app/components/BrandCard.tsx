4eb9951139be5a4e455c826ec33bde8522b1b666
CiaraMaria <mariaverse@protonmail.com>
2024-10-10 08:30:46 -0600

    update card to accept props

--- src/app/components/BrandCard.tsx
+++ src/app/components/BrandCard.tsx
@@ -3,16 +3,22 @@ import React from 'react';
 
 import Text from './Text';
 
-const BrandCard = () => {
+interface BrandCardProps {
+  brandTitle: string;
+  brandParagraph: string;
+  logo: string;
+}
+
+
+const BrandCard: React.FC<BrandCardProps> = ({ brandTitle, brandParagraph, logo }) => {
   return (
-    <div className="flex flex-col items-center justify-center text-center w-[296px] h-[318px] md:h-[466px] gap-5">
-        <Image alt="Brand" src="/house-brands/antonio.webp" width={210} height={212} />
+    <div className="flex flex-col items-center justify-center text-center w-[296px] h-[318px] md:h-[466px] gap-2">
+        <Image className="w-[150px] xl:w-[200px]" alt="Brand" src={logo} width={200} height={200} />
         <Text variant="headlineSm" className="uppercase">
-            Brand Name
+            {brandTitle}
         </Text>
         <Text variant="paragraph" className="text-darkest/70">
-            Quality products are the world&apos;s best internationally sourced ingredients. 
-            Bellissimo products range from cheese, meat toppings, and tomatoes, to cleaning supplies and disposables.
+            {brandParagraph}
         </Text>
     </div>
   );
