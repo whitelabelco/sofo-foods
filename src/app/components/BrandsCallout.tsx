2a733e7a2fd7650d6eeeb5cab4019e8b9ea18d45
CiaraMaria <mariaverse@protonmail.com>
2024-10-09 20:18:01 -0600

    refactor callouts to be reusable for variations

--- src/app/components/BrandsCallout.tsx
+++ src/app/components/BrandsCallout.tsx
@@ -7,7 +7,12 @@ import Text from './Text';
 import Button from './Button';
 import Image from 'next/image';
 
-const BrandsCallout = () => {
+interface BrandsCalloutProps {
+    backgroundImage?: boolean;
+
+}
+
+const BrandsCallout: React.FC<BrandsCalloutProps> = ({ backgroundImage = false }) => {
     const router = useRouter()
 
     const brandsParagraph = `Our pride and dedication to quality Italian foods led us to create our own family of brands which we have been developing over the decades. 
@@ -24,31 +29,32 @@ const BrandsCallout = () => {
     ]
 
     return (
-        <section className="flex flex-col gap-12 justify-center items-center h-[564px] md:h-[622px] mt-4 md:mt-0 overflow-hidden">
+        <section className={backgroundImage ? "flex flex-col gap-12 justify-center items-center h-[564px] md:h-[622px] mt-4 md:mt-0 overflow-hidden bg-brands-hero bg-cover bg-right" : "flex flex-col gap-12 justify-center items-center h-[564px] md:h-[622px] mt-4 md:mt-0 overflow-hidden"}>
             <div className="flex flex-col w-[342px] md:w-[674px] justify-center items-center gap-8">
                 <div className="flex flex-col justify-center items-center text-center gap-4">
-                <Text variant="headlineXl">Brands</Text>
-                <Text variant="paragraph" className="text-darkest/70">{brandsParagraph}</Text>
+                <Text variant="headlineXl" className={backgroundImage ? "text-white" : ""}>Brands</Text>
+                <Text variant="paragraph" className={backgroundImage ? "text-white" : "text-darkest/70"}>{brandsParagraph}</Text>
                 </div>
                 <div className="flex flex-col md:flex-row justify-center items-center gap-6">
                 <span className="inline-flex">
-                    <Button onClick={() => router.push('/brands')} variant="red">More about brands</Button>
+                    <Button onClick={() => router.push('/brands')} variant={backgroundImage ? "blackText" : "red"}>More about brands</Button>
                 </span>
                 <span className="inline-flex">
-                    <Button onClick={() => router.push('/order')} variant="transparentBlack">Start your first order</Button>
+                    <Button onClick={() => router.push('/order')} variant={backgroundImage ? "transparentWhite" : "transparentBlack"}>Start your first order</Button>
                 </span>
                 </div>
             </div>
-
-            <div className="flex flex-row w-full max-w-screen-2xl">
-                <div className="grid grid-cols-7 gap-44 md:gap-40 lg:gap-24">
-                    {featuredBrands.map((brand, index) => (
-                        <div key={index} className="flex justify-center items-center">
-                            <Image className="max-w-none" src={brand} alt="Brand" width={172} height={172} />
-                        </div>
-                    ))}
+            { !backgroundImage && (
+                <div className="flex flex-row w-full max-w-screen-2xl">
+                    <div className="grid grid-cols-7 gap-44 md:gap-40 lg:gap-24">
+                        {featuredBrands.map((brand, index) => (
+                            <div key={index} className="flex justify-center items-center">
+                                <Image className="max-w-none" src={brand} alt="Brand" width={172} height={172} />
+                            </div>
+                        ))}
+                    </div>
                 </div>
-            </div>
+            )}
         </section>
     );
 };
--- src/app/components/ProductsCallout.tsx
+++ src/app/components/ProductsCallout.tsx
@@ -6,27 +6,65 @@ import { useRouter } from 'next/navigation'
 import Text from './Text';
 import Button from './Button';
 
-const ProductsCallout = () => {
+interface ProductsCalloutProps {
+    variant?: 'single' | 'double';
+    firstButtonRoute?: string;
+    secondButtonRoute?: string;
+    title: string;
+    paragraph: string;
+    firstButtonText?: string;
+    secondButtonText?: string;
+    firstButtonVariant?: 'red' | 'blue' | 'green' | 'redText' | 'blackText' | 'blueText' | 'transparentBlack' | 'transparentWhite' | 'product';
+    secondButtonVariant?: 'red' | 'blue' | 'green' | 'redText' | 'blackText' | 'blueText' | 'transparentBlack' | 'transparentWhite' | 'product';
+}
+
+const ProductsCallout: React.FC<ProductsCalloutProps> = ({ variant = "single", firstButtonRoute = "/", secondButtonRoute = "/", title, paragraph, firstButtonText, secondButtonText, firstButtonVariant, secondButtonVariant }) => {
     const router = useRouter()
 
+    const common = (
+        <div className="flex flex-col justify-center items-center text-center gap-4">
+            <Text variant="headlineXl" className={variant === "double" ? "text-white" : ""}>{title}</Text>
+            <Text variant="paragraph" className={variant === "double" ? "text-white" : "text-darkest/70"}>{paragraph}</Text>
+        </div>
+    )
+
+    const firstButton = (
+        <span className="inline-flex">
+            <Button onClick={() => router.push(`${firstButtonRoute}`)} variant={firstButtonVariant}>{firstButtonText}</Button>
+        </span>
+    )
+
+    const secondButton = (
+        <span className="inline-flex">
+            <Button onClick={() => router.push(`${secondButtonRoute}`)} variant={secondButtonVariant}>{secondButtonText}</Button>
+        </span>
+    )
+
     return (
-        <section className="flex flex-col justify-center items-center overflow-hidden ">
-            <div className="bg-products-callout bg-cover bg-top w-full h-[140px] md:h-[282px] lg:h-[650px] z-0"></div>
-            <div className="flex flex-col justify-center items-center gap-8 w-[342px] sm:w-[450px] md:w-[914px] z-10">
-                <div className="flex flex-col justify-center items-center text-center gap-4">
-                <Text variant="headlineXl">Products</Text>
-                <Text variant="paragraph" className="text-darkest/70">
-                    Our pride and dedication to quality Italian foods led us to create our own family of brands which we have been developing over the decades. 
-                    Each of our family brands represents a commitment to quality, flavor and consistency.
-                </Text>
+        <>
+        { variant === 'single' && (
+            <section className="flex flex-col justify-center items-center overflow-hidden">
+                <div className="bg-products-callout bg-cover bg-top w-full h-[140px] md:h-[282px] lg:h-[520px] xl:h-[700px] z-0"></div>
+                <div className="flex flex-col justify-center items-center gap-8 w-[342px] sm:w-[450px] md:w-[914px] z-10">
+                    {common}
+                    <div className="flex flex-row justify-center items-center gap-6">
+                        {firstButton}
+                    </div>
                 </div>
-                <div className="flex flex-row justify-center items-center gap-6">
-                <span className="inline-flex">
-                    <Button onClick={() => router.push('/products')} variant="red">All products</Button>
-                </span>
+            </section>
+        )}
+        { variant === 'double' && (
+            <section className="flex flex-col gap-12 justify-center items-center h-[564px] md:h-[622px] mt-4 md:mt-0 overflow-hidden bg-products-hero bg-cover bg-right">
+            <div className="flex flex-col w-[342px] md:w-[674px] justify-center items-center gap-8">
+                {common}
+                <div className="flex flex-col md:flex-row justify-center items-center gap-6">
+                    {firstButton}
+                    {secondButton}
                 </div>
             </div>
-        </section>
+            </section>
+        )}
+        </>
     );
 };
 
