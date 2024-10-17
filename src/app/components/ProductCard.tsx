2221a1a8a9f489332408dbfe3c12f3e6651957c8
CiaraMaria <mariaverse@protonmail.com>
2024-10-17 10:35:19 -0600

    create re-usable products grid

--- src/app/components/ProductCard.tsx
+++ src/app/components/ProductCard.tsx
@@ -22,7 +22,7 @@ const ProductCard: React.FC<ProductCardProps> = ({ name, route, className }) => 
 
 
   return (
-    <div onClick={handleClick} className={`${className} group bg-product-placeholder bg-cover w-[180px] h-[148px] md:w-[450px] md:h-[300px] flex flex-col relative gap-6 rounded-lg md:rounded-2xl cursor-pointer`}>
+    <div onClick={handleClick} className={`${className} group bg-cover w-[180px] h-[148px] md:w-[450px] md:h-[300px] flex flex-col relative gap-6 rounded-lg md:rounded-2xl cursor-pointer`}>
         <div className="bg-darkest bg-opacity-0 p-4 w-full h-full hover:bg-opacity-50 transition-all duration-200 rounded-lg md:rounded-2xl">
             < div className="absolute bottom-0 left-0">
               <Button onClick={handleClick} variant="product" className="!px-2 lg:!px-4">{name}</Button>
